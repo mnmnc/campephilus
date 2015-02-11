12a002cc88b5dcc0e7e57fe18b156271d0722265
mnmnc <chooseright+git@gmail.com>
2015-02-11 11:40:48 +0100

    LOF working. Stage 1.
    Improvements comming soon.

--- modules/detector/lof2d.py
+++ modules/detector/lof2d.py
@@ -13,6 +13,9 @@ class LOF2D:
 		self.neighbours = {}
 		self.outliers = []
 		self.k_distances = {}
+		self.lrd = {}
+		self.reach_distance_sum = {}
+		self.lof = {}
 
 	def create_distance_dictionary(self):
 		"""
@@ -85,10 +88,38 @@ class LOF2D:
 			for subkey in self.k_distances[key]:
 				print("\t",subkey,"\t:", self.k_distances[key][subkey])
 
+	def calculate_lrd(self):
+		for point in self.k_distances:
+			neighbours_count = len(self.k_distances[point])
+
+			sum = 0
+			for neighbour in self.k_distances[point]:
+				biggest = -1
+				for inner_neighbour in self.k_distances[neighbour]:
+					if self.k_distances[neighbour][inner_neighbour] > biggest:
+						biggest = self.k_distances[neighbour][inner_neighbour]
+
+
+				dist = self.neighbours[neighbour][point]
+
+				if biggest > dist:
+					sum += biggest
+				else:
+					sum += dist
+
+			self.lrd.update({point: neighbours_count/sum})
+			self.reach_distance_sum.update({point:sum})
+
+	def calculate_lof(self):
+		for point in self.k_distances:
+			lrd_sum = 0
+			for neighbour in self.k_distances[point]:
+				lrd_sum += self.lrd[neighbour]
+			print("LOF", point, lrd_sum * self.reach_distance_sum[point])
 
 def main():
 
-
+	neighbours = 3
 
 	points = [
 		[1, 2], [2, 4], [3, 6], [4, 37], [5, 2],
@@ -103,46 +134,66 @@ def main():
 	lof.print_neighbours()
 
 	# 2. Get all neighbours that are closer or equal to k neighbour
-	lof.get_knn(3)
+	lof.get_knn(neighbours)
 
 	lof.print_k_distances()
 
 	# 3. Calculate local reachability density for all points
 
+	lrd = {}
+	reachdist_sum = {}
+
 	# FOR [1, 2] - first point
-
+	for t_point in lof.k_distances:
 		# Get number of neighbours
-	point_neighbours_count = len(lof.k_distances[(1,2)])
-	print("Neighbours count:", point_neighbours_count)
+		point_neighbours_count = len(lof.k_distances[t_point])
+		#print("Neighbours count:", point_neighbours_count)
 
-		# For each neighbour calculate reachdist
-			# maximum from
-			#               distance from neighbour to its k neighbour
-			#               or
-			#               distance from neighbour to original point
-	sum = 0
-	for point in lof.k_distances[(1,2)]:
-		print("Calculating reachdist for", point)
+			# For each neighbour calculate reachdist
+				# maximum from
+				#               distance from neighbour to its k neighbour
+				#               or
+				#               distance from neighbour to original point
+		sum = 0
+		for point in lof.k_distances[t_point]:
+			#print("Calculating reachdist for", point)
 
-		biggest = -1
-		for neighbour in lof.k_distances[point]:
-			if lof.k_distances[point][neighbour] > biggest:
-				biggest = lof.k_distances[point][neighbour]
+			biggest = -1
+			for neighbour in lof.k_distances[point]:
+				if lof.k_distances[point][neighbour] > biggest:
+					biggest = lof.k_distances[point][neighbour]
 
-		print("\tBiggest:", biggest )
+			#print("\tBiggest:", biggest )
 
-		dist = lof.neighbours[point][(1,2)]
+			dist = lof.neighbours[point][t_point]
 
-		print("\tDistance", point, "to (1,2):", dist)
+			#print("\tDistance", point, "to", t_point, ":", dist)
 
-		if biggest > dist:
-			sum += biggest
-		else:
-			sum += dist
+			if biggest > dist:
+				sum += biggest
+			else:
+				sum += dist
 
-	print("Sum: ", sum)
-	print("LRD: ", point_neighbours_count/sum)
+
+		#print("Sum: ", sum)
+		print("LRD of ", t_point , ":", point_neighbours_count/sum)
+		lrd.update({t_point: point_neighbours_count/sum})
+		reachdist_sum.update({t_point:sum})
+
+	lof.calculate_lrd()
+
 	# 4. Calculate LOF
+
+	for t_point in lof.k_distances:
+		#print("LOF", t_point)
+		lrd_sum = 0
+		for neighbour in lof.k_distances[t_point]:
+			#print("\tNeighbour", neighbour, "lrd", lrd[neighbour])
+			lrd_sum += lrd[neighbour]
+		#print("\tSum", reachdist_sum[t_point])
+		print("LOF", t_point, lrd_sum * reachdist_sum[t_point])
+
+	lof.calculate_lof()
 
 	# 5. Sort
 
